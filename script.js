c5203cceaf844b0cd88a1c8f67babef3964073ec
Balazs Szanka <szankabalazs@gmail.com>
2021-09-11 12:17:59 +0200

    added a div for results instead of alerts. added some inline css.

--- script.js
+++ script.js
@@ -11,20 +11,31 @@ function play(playersValue) {
     var player = playersValue * 1;
     var enemy = randomNumber() * 1;
     // alert(player + " vs " + enemy)
-    if (player == enemy){
-        alert("tie!");
+    if (player == enemy) {
+        document.getElementById("result").textContent = getEnemy(enemy) + "Döntetlen! 🤝";
         document.stats.ties.value++;
     }
     else {
         if (player == 0 && enemy == 2
             || player == 1 && enemy == 0
-            || player == 2 && enemy == 1){
-            alert("win!");
+            || player == 2 && enemy == 1) {
+            document.getElementById("result").textContent = getEnemy(enemy) + "Nyertél! 💪";
             document.stats.wins.value++;
         }
-        else{
-            alert("lose!");
+        else {
+            document.getElementById("result").textContent = getEnemy(enemy) + "Vesztettél! 👎";
             document.stats.loses.value++;
         }
     }
+}
+
+function getEnemy(x) {
+    if (x == 0)
+        return "Kő! ";
+    else if (x == 1)
+        return "Papír! ";
+    else if (x == 2)
+        return "Olló! ";
+    else
+        return "Ajaj. ";
 }
